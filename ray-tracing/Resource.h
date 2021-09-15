ba7a9fc306d0705d8d044c30469b635288b03073
DESKTOP-FMII11R\zi-ku <program922@hotmail.com>
2021-09-16 07:44:12 +0900

    様々なダイアログボックスを使用してみた． メインウィンドウには出力されないからそこだけ何とかしたい． CombBoxやListBoxにはリソースファイルに値を入れとくことができない…． プログラムのどこかで設定しておく必要がある．

--- ray-tracing/Resource.h
+++ ray-tracing/Resource.h
@@ -10,32 +10,42 @@
 #define IDM_EXIT                        105
 #define IDI_RAYTRACING                  107
 #define IDI_SMALL                       108
+#define My_DLG                          108
 #define IDC_RAYTRACING                  109
-#define IDC_CLOSE						110
-#define IDM_DLG							111
-#define IDM_CLOSEDLG					112
-#define IDM_END							113
-#define IDC_EDIT1						114
-#define MYDLG							115
+#define IDM_DLG                         111
+#define IDM_CLOSEDLG                    112
+#define IDM_END                         113
 #define IDR_MAINFRAME                   128
 #define JPGmana                         133
 #define PNGmana                         136
 #define IDB_BITMAP1                     139
 #define IDB_BITMAP2                     140
 #define IDR_ACCELERATOR1                141
+#define male                            1001
+#define female                          1002
+#define japan                           1003
+#define not_japan                       1004
+#define twenty_over                     1005
+#define prefectures                     1006
+#define animal                          1008
+#define IDC_RADIO1                      1009
+#define phone_num                       1010
+#define scroll                          1012
+#define scroll1                         1012
+#define IDC_COMBO1                      1013
 #define ID_PICTURE_32771                32771
 #define ID_PICTURE_32772                32772
+#define ID_32774                        32774
 #define IDC_STATIC                      -1
-#define IDC_STATIC1						-1
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        142
-#define _APS_NEXT_COMMAND_VALUE         32774
-#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_RESOURCE_VALUE        143
+#define _APS_NEXT_COMMAND_VALUE         32775
+#define _APS_NEXT_CONTROL_VALUE         1014
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
 #endif
--- ray-tracing/ray-tracing.cpp
+++ ray-tracing/ray-tracing.cpp
@@ -4,6 +4,8 @@
 #include "framework.h"
 #include "ray-tracing.h"
 #include <time.h>
+#include <windows.h>
+#include <windowsx.h>
 
 #define MAX_LOADSTRING 100
 
@@ -19,9 +21,8 @@ LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK MyDlgProc(HWND, UINT, WPARAM, LPARAM);
 
-HWND hDlg;
-TCHAR szName[32];
-HWND hMain;
+TCHAR szText[6][64];
+int nSex, nNation, n20, nPet, nAddress, nPos = 50;
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
@@ -47,22 +48,14 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_RAYTRACING));
 
     MSG msg;
-	BOOL bRet;
 
     // メイン メッセージ ループ:
-    while ((bRet = GetMessage(&msg, nullptr, 0, 0)) != 0)
+    while (GetMessage(&msg,nullptr,0,0))
     {
-		if (bRet == -1) {
-			break;
-		}
-		else {
-			if (!hDlg || !IsDialogMessage(hDlg, &msg)) {
-				if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg)) {
-					TranslateMessage(&msg);
-					DispatchMessage(&msg);
-				}
-			}
-		}
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg)) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+        }
     }
 
     return (int) msg.wParam;
@@ -120,7 +113,6 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 
    ShowWindow(hWnd, nCmdShow);
    UpdateWindow(hWnd);
-   hMain = hWnd;
    return TRUE;
 }
 
@@ -139,9 +131,6 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	HDC hdc;
     PAINTSTRUCT ps;
     
-    static HMENU hMenu;
-	TCHAR szBuf[64];
-
     switch (message) {
 		case WM_COMMAND: {
 			int wmId = LOWORD(wParam);
@@ -153,54 +142,19 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 			case IDM_EXIT:
 				DestroyWindow(hWnd);
 				break;
+			case IDM_DLG:
+				//MessageBox(hWnd, TEXT("選択はされました"), TEXT("謎"), MB_OK);
+				DialogBox(hInst, MAKEINTRESOURCE(My_DLG), hWnd, MyDlgProc);
+				break;
 			case IDM_END:
 				SendMessage(hWnd, WM_CLOSE, 0, 0);
-				break;
-			case IDM_DLG:
-				//DialogBox(hInst, MAKEINTRESOURCE(MYDLG), hWnd, MyDlgProc);
-				hDlg = CreateDialog(hInst, MAKEINTRESOURCE(MYDLG), hWnd, MyDlgProc);
-				ShowWindow(hDlg, SW_NORMAL);
-				break;
-			case IDM_CLOSEDLG:
-				DestroyWindow(hDlg);
 				break;
 			default:
 				return DefWindowProc(hWnd, message, wParam, lParam);
 			}
 		} break;
-	case WM_CLOSE:
-		if (IsWindow(hWnd)) {
-			MessageBox(hWnd, TEXT("ダイアログを破棄します"), TEXT("破棄"), MB_OK);
-			DestroyWindow(hDlg);
-        }
-		DestroyWindow(hWnd);
-        break;
-	case WM_CREATE:
-		hMenu = GetMenu(hWnd);
-		break;
-	case WM_INITMENU:
-		if (IsWindow(hDlg)) {
-			EnableMenuItem(hMenu, IDM_DLG, MF_BYCOMMAND | MF_GRAYED);
-			EnableMenuItem(hMenu, IDM_CLOSEDLG, MF_BYCOMMAND | MF_ENABLED);
-
-        }
-		else {
-			EnableMenuItem(hMenu, IDM_DLG, MF_BYCOMMAND | MF_ENABLED);
-			EnableMenuItem(hMenu, IDM_CLOSEDLG, MF_BYCOMMAND | MF_GRAYED);
-        }
-		DrawMenuBar(hWnd);
-        break;
-
 	case WM_PAINT: {
-		if (lstrcmp(szName, TEXT("")) == 0) {
-			lstrcpy(szBuf, TEXT("まだ名前の入力が有りません"));
-		}
-		else {
-			wsprintf(szBuf, TEXT("入力された氏名は%sさんです"), szName);
-		}
-        hdc = BeginPaint(hWnd, &ps);
-		TextOut(hdc, 10, 10, szBuf, lstrlen(szBuf));
-		
+		BeginPaint(hWnd, &ps);
 		// TODO: HDC を使用する描画コードをここに追加してください.
 		EndPaint(hWnd, &ps);
 	} break;
@@ -234,29 +188,197 @@ INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
     return (INT_PTR)FALSE;
 }
 
-INT_PTR CALLBACK MyDlgProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam) {	
-	static HWND hParent;
-	switch (message) {
-		case WM_INITDIALOG:
-			hParent = GetParent(hDlg);
-			return (INT_PTR)TRUE;
-			
-		case WM_COMMAND: {
-			switch (LOWORD(wParam)) {
-			case IDOK:
-				GetDlgItemText(hDlg, IDC_EDIT1, szName, (int)sizeof(szName) - 1);
-				DestroyWindow(hDlg);
-				InvalidateRect(hParent, NULL, TRUE);
-				return TRUE;
-			case IDCANCEL:
-				SetDlgItemText(hDlg, IDC_EDIT1, TEXT(""));
-				return TRUE;
-			case IDC_CLOSE:
-				DestroyWindow(hDlg);
-				return TRUE;
-			}
+INT_PTR CALLBACK MyDlgProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam) {
+	static HWND hMale, hFemale, hJapan, hNot_japan;
+	static HWND hTwenty_over,hPrefecture, hAnimal, hPhone_num, hScroll;
+	TCHAR szBuf[64];
+	TCHAR szAnimal[5][16] = { TEXT("飼っていない"),
+		TEXT("犬"),
+		TEXT("猫"),
+		TEXT("ネズミ"),
+		TEXT("馬") };
+	TCHAR szPrefecture[6][16] = { TEXT("北海道"),
+		TEXT("本州"),
+		TEXT("四国"),
+		TEXT("九州"),
+		TEXT("沖縄"),
+		TEXT("その他") };
+	static HWND hMain;
+	int id, n;
+	BOOL bSuccess;
+
+    switch (message) {
+	case WM_HSCROLL:
+		if (lParam != (LPARAM)hScroll)
+			return FALSE;
+		switch (LOWORD(wParam)) {
+		case SB_LEFT:
+			nPos = 0;
+			break;
+		case SB_RIGHT:
+			nPos = 100;
+			break;
+		case SB_LINELEFT:
+			nPos--;
+			if (nPos < 0)
+				nPos = 0;
+            break;
+		case SB_LINERIGHT:
+			nPos++;
+			if (nPos > 100)
+				nPos = 100;
+			break;
+		case SB_PAGELEFT:
+			nPos -= 10;
+			if (nPos < 0)
+				nPos = 0;
+			break;
+		case SB_PAGERIGHT:
+			nPos += 10;
+			if (nPos > 100)
+				nPos = 100;
+			break;
+		case SB_THUMBTRACK:
+		case SB_THUMBPOSITION:
+			nPos = HIWORD(wParam);
+			break;
+		default:
+			break;
 		}
-	return FALSE;
-    }
-return FALSE;	
+		//スクロールバーの移動
+		ScrollBar_SetPos(hScroll, nPos, TRUE);
+
+		wsprintf(szBuf, TEXT("%03d"), nPos);
+		Edit_SetText(hPhone_num,szBuf);
+		wsprintf(szText[5], TEXT("スクロールバーの位置 = %03d", nPos));
+		InvalidateRect(hMain, NULL, TRUE);
+		return TRUE;
+	case WM_INITDIALOG:
+		//親ウィンドウのハンドル取得
+		hMain = GetParent(hDlg);
+		//各コントロールのハンドル取得
+		hMale = GetDlgItem(hDlg, male);
+		hFemale = GetDlgItem(hDlg, female);
+		hJapan = GetDlgItem(hDlg, japan);
+		hNot_japan = GetDlgItem(hDlg, not_japan);
+		hTwenty_over = GetDlgItem(hDlg, twenty_over);
+		hAnimal = GetDlgItem(hDlg, animal);
+		hPrefecture = GetDlgItem(hDlg, prefectures);
+		hPhone_num = GetDlgItem(hDlg, phone_num);
+		hScroll = GetDlgItem(hDlg, scroll1);
+		//スクロールバーの初期設定
+		ScrollBar_SetRange(hScroll, 0, 100, TRUE);
+		ScrollBar_SetPos(hScroll, nPos, TRUE);
+		//ラジオグループの初期設定
+		if (nSex == 0)
+			Button_SetCheck(hMale,BST_CHECKED);
+		else
+			Button_SetCheck(hFemale, BST_CHECKED);
+		if (nNation == 0)
+			Button_SetCheck(hJapan, BST_CHECKED);
+		else
+			Button_SetCheck(hNot_japan, BST_CHECKED);
+		//チェックボックスの設定
+		if (n20 == 1)
+			Button_SetCheck(hTwenty_over, BST_CHECKED);
+		else
+			Button_SetCheck(hTwenty_over, BST_CHECKED);
+		//コンボボックスへ文字列を与える
+		for (n = 0; n < 6; n++)
+			ComboBox_AddString(hPrefecture, szPrefecture[n]);
+		//リストボックスへ文字列を与える
+		for (n = 0; n < 5; n++)
+			ListBox_AddString(hAnimal, szAnimal[n]);
+		//コンボボックスの初期値
+		ComboBox_SetCurSel(hPrefecture, nAddress);
+		ListBox_SetCurSel(hAnimal, nPet);
+		wsprintf(szBuf, TEXT(" % 03d"), nPet);
+		Edit_SetText(hPhone_num, szBuf);
+		return TRUE;
+	case WM_COMMAND:
+		switch (LOWORD(wParam)) {
+		case IDCANCEL:
+			EndDialog(hDlg, IDCANCEL);
+			return TRUE;
+		case IDOK:
+			if (nSex == 0)
+				lstrcpy(szText[0], TEXT("男が選択されました"));
+			else
+				lstrcpy(szText[0], TEXT("女が選択されました"));
+			if (nNation == 0)
+				lstrcpy(szText[1], TEXT("日本が選択されました"));
+			else
+				lstrcpy(szText[1], TEXT("日本以外が選択されました"));
+			if (n20 == 0)
+				lstrcpy(szText[2], TEXT("20歳未満です"));
+			else
+				lstrcpy(szText[2], TEXT("20歳以上です"));
+			wsprintf(szText[3], TEXT("%sが選択されました"), szPrefecture[nAddress]);
+			wsprintf(szText[4], TEXT("%sが選択されました"), szAnimal[nPet]);
+			wsprintf(szText[5], TEXT("スクロールバーの位置 = %03d"), nPos);
+			InvalidateRect(hMain, NULL, TRUE);
+			EndDialog(hDlg, IDOK);
+			return TRUE;
+		case male:
+			nSex = 0;
+			lstrcpy(szText[0], TEXT("男が選択されました"));
+			InvalidateRect(hMain, NULL, TRUE);
+			return TRUE;
+		case female:
+			nSex = 1;
+			lstrcpy(szText[0], TEXT("女が選択されました"));
+			InvalidateRect(hMain, NULL, TRUE);
+			return TRUE;
+		case japan:
+			nNation = 0;
+			lstrcpy(szText[1], TEXT("日本が選択されました"));
+			InvalidateRect(hMain, NULL, TRUE);
+			return TRUE;
+		case not_japan:
+			nNation = 1;
+			lstrcpy(szText[1], TEXT("日本以外が選択されました"));
+			InvalidateRect(hMain, NULL, TRUE);
+			return TRUE;
+		case twenty_over:
+			if (Button_GetCheck(hTwenty_over) == BST_CHECKED) {
+				n20 = 1;
+				lstrcpy(szText[2], TEXT("20歳以上です"));		
+			}
+			else {
+				n20 = 0;
+				lstrcpy(szText[2], TEXT("20歳未満です"));		
+			}
+			InvalidateRect(hMain, NULL, TRUE);
+			return TRUE;
+		case prefectures:
+			id = ComboBox_GetCurSel(hPrefecture);
+			wsprintf(szText[3], TEXT("%sが選択されました"), szPrefecture[id]);
+			InvalidateRect(hDlg, NULL, TRUE);
+			nAddress = id;
+			return TRUE;
+		case animal:
+			id = ListBox_GetCurSel(hAnimal);
+			wsprintf(szText[4], TEXT("%sが選択されました"), szAnimal[id]);
+			InvalidateRect(hDlg, NULL, TRUE);
+			nPet = id;
+			return TRUE;
+		case phone_num:
+			nPos = GetDlgItemInt(hDlg, phone_num, &bSuccess, FALSE);
+			if (nPos > 100)
+				nPos = 100;
+			if (nPos < 0)
+				nPos = 0;
+			ScrollBar_SetPos(hScroll, nPos, TRUE);
+			wsprintf(szText[5], TEXT("スクロールバーの位置 = %03d"), nPos);
+			InvalidateRect(hDlg, NULL, TRUE);
+			return TRUE;
+		default:
+			break;
+		}
+	default:
+		break;
+	return (INT_PTR)FALSE;
+	}
+
+    return (INT_PTR)FALSE;
 }
